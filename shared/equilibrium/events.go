8035cb3b99daa26a3c0c57761c49be0de06e95ca
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2020-11-03 15:21:40 +0300

    Add Reason field to EventBalancesTransfer

--- shared/equilibrium/events.go
+++ shared/equilibrium/events.go
@@ -4,6 +4,7 @@ import (
 	"github.com/centrifuge/go-substrate-rpc-client/types"
 )
 
+/// Defines the currency of a transfer.
 type Currency byte
 const (
 	Unknown Currency = 0
@@ -14,6 +15,17 @@ const (
 	Eos     Currency = 5
 )
 
+/// Describes the reason for a transfer.
+type Reason byte
+const(
+	Common                 Reason = 0
+	InterestFee            Reason = 1
+	MarginCall             Reason = 2
+	BailsmenRedistribution Reason = 3
+	TreasuryEqBuyout       Reason = 4
+	TreasuryBuyEq          Reason = 5
+)
+
 // EventBalancesTransfer is emitted when a Substrate client calls Currency::transfer.
 type EventBalancesTransfer struct {
 	Phase    types.Phase
@@ -21,5 +33,6 @@ type EventBalancesTransfer struct {
 	To       types.AccountID
 	Currency Currency
 	Value    types.U64
+	Reason   Reason
 	Topics   []types.Hash
 }
